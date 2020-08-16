99f2077aac1aef85abfe242022fde97c79aa5625
YutaTakata <yuta.takata24@gmail.com>
2020-08-16 11:35:48 +0900

    Retry to solve D problem

--- atcoder/ABC/abc175/d/main.py
+++ atcoder/ABC/abc175/d/main.py
@@ -5,33 +5,30 @@ def main():
     P = list(map(int, input().split()))
     C = list(map(int, input().split()))
     P = [i-1 for i in P]
-    C = [i-1 for i in C]
+    ans = -1e19
 
-    score_table = [[0 for i in range(N)] for _ in range(N)]
-    maxv = - int(1e9 + 100)
-    max_index = 0
-    max_value = 0
-    for i in range(N):
-        count = 0
-        index = i
-        while count < N-1:
+    for si in range(N):
+        index = si
+        cycle =[]
+        total = 0
+        while True:
             index = P[index]
-            score_table[i][count + 1] = score_table[i][count] + C[index]
-            maxv = max(maxv, score_table[i][count+1])
-            if index == i:
-                if score_table[i][count + 1] > 0 and score_table[i][count + 1] > max_value:
-                    max_index = i
-                    max_value = score_table[i][count + 1]
+            cycle.append(C[index])
+            total = total + C[index]
+            if index == si:
                 break
-            count = count + 1
-    if K <= N:
-        print(maxv)
-    else:
-        resd = K % N
-        cnt = K // N
-        addm = 0
-        for i in range(0, resd):
-            addm = max(addm, score_table[max_index][i])
-        print(cnt * max_value + addm)
+        cycle_length = len(cycle)
+        count = 0
+        for i in range(cycle_length):
+            count  = count + cycle[i]
+            if i+1 > K:
+                break
+            num = count
+            if total > 0:
+                cnt = (K - (i+1)) // cycle_length
+                num = num + cnt*total
+            ans = max(ans, num)
+    print(ans)
+
 
 main()
